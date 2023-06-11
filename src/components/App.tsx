9b583ade2025a3bb263498bcea38a903691b7947
Hex09AF <lsqhuy@gmail.com>
2023-06-11 12:24:53 +0700

    :sparkles: refetchable search list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import { Suspense } from "react";
 import LoadingSpinner from "./LoadingSpinner";
 import Newsfeed from "./Newsfeed";
+import Sidebar from "./Sidebar";
 
 export default function App(): React.ReactElement {
   return (
     <Suspense fallback={<LoadingSpinner />}>
       <div className="app">
         <Newsfeed />
+        <Sidebar />
       </div>
     </Suspense>
   );
